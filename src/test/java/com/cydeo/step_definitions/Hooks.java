dc3434efb739dd31556b3ee575c98cd57df2e24a
Senai <simoseni@gmail.com>
2023-08-23 10:33:07 -0700

    Initial commit

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,47 @@
+package com.cydeo.step_definitions;
+
+/*
+In the class we will be able to pass pre- & post- conditions to
+ each scenario and each step
+ */
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    //import from io.cucumber.java not from junit
+    @Before
+    public void setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+
+
+
+    @After
+    public void teardownScenario(Scenario scenario){
+
+        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
+
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+
+        //BrowserUtils.sleep(5);
+        Driver.closeDriver();
+
+        //System.out.println("====Closing browser using cucumber @After");
+        //System.out.println("====Scenario ended/ Take screenshot if failed!");
+    }
+
+
+
+
+}
